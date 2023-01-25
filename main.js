2de0f34452a31979f65ef294cc37e37e10396fa6
MadCauseCode <123124431+MadCauseCode@users.noreply.github.com>
2023-01-26 00:39:47 +0200

    added functionality

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const rl = readline.createInterface({
 
 function run(currentContacts)
 {
-    rl.question('Hello, welcome to the contacts app. What would you like to do? view, search, edit, add, remove or search? ', (action) => {
+    rl.question('Hello, welcome to the contacts app. What would you like to do? view, search, edit, edit number, add, remove? ', (action) => {
         if(action.toLowerCase() === "view")
         {
             console.log("here is your Contact list: ");
@@ -39,16 +39,34 @@ function run(currentContacts)
         }
         else if (action.toLowerCase() === "add")
         {
-            rl.question("What is the name of the contact? \n", (name) => { //user input for contact's name
-                rl.question("What is the number of the contact? ", (number) => {  // user input for contact's number
-                    let contact = name + " - " + number; // adding the name and number with a - in between to a new value called contact
-                    currentContacts += contact + "\n"; //adds contact to currentContact list with a new line in between
-                    fs.writeFile("contacts.txt", currentContacts, (err) =>{
-                        if (err) throw err;
-                        console.log("Contact Saved Successfully!");
-                        run(currentContacts); //throws the user back to the main screen make another action
+            rl.question("What is the name of the contact? \n", (newName) => { //user input for contact's name
+                const contactList=currentContacts.toString().split("\n");
+                const existingContact = contactList.find(contact => contact.startsWith(newName));
+                if(existingContact)
+                {
+                   console.log("the name you have entered already exists, try again...");
+                   run(currentContacts);
+                } 
+                else
+                {
+                    rl.question("What is the number of the contact? ", (number) => {  // user input for contact's number
+                        if(number.length === 10)
+                        {
+                            let contact = newName + " - " + number; // adding the name and number with a - in between to a new value called contact
+                            currentContacts += contact + "\n"; //adds contact to currentContact list with a new line in between
+                            fs.writeFile("contacts.txt", currentContacts, (err) =>{
+                                if (err) throw err;
+                                console.log("Contact Saved Successfully!");
+                                run(currentContacts); //throws the user back to the main screen make another action
+                            })
+                        }
+                        else
+                        {
+                            console.log("Invalid Number! should be 10 digits!")
+                            run(currentContacts); //throws the user back to the main screen make another action
+                        }
                     })
-                })
+                }
             })
         }
         else if(action.toLowerCase() === "remove")
@@ -77,6 +95,36 @@ function run(currentContacts)
                 })
             })
         }
+        else if(action.toLowerCase() === "edit number")
+        {
+            rl.question("Which contact's number would you like to edit? \n", (nameToFind) => {
+                const contactList = currentContacts.split("\n"); // splits to array
+                let contactIndex = contactList.findIndex(contact => contact.startsWith(nameToFind)); //find the index in the array of the contnact by nameToFind
+                if(contactIndex === -1) { //checks if the conntact doesnt exist, send back to main screen for reattempt
+                    console.log("the contact you are trying to edit does not exist!");
+                    run(currentContacts);
+                } 
+                else 
+                {
+                    rl.question("what is the new number for " + nameToFind + "? ", (newNumber) => { // if does exist, ask for new number
+                        if(newNumber.length === 10)
+                        {
+                          contactList[contactIndex] = nameToFind + " - " + newNumber; // edits contact info in the contact list
+                          currentContacts = contactList.join("\n"); // joins array bacvk to string list
+                           fs.writeFile("contacts.txt", currentContacts, (err) => { 
+                             if (err) throw err;
+                               console.log("this contact has been updated successfully! \n");
+                            });
+                        }
+                        if(newNumber.length != 10)
+                        {
+                            console.log("Invalid Number! should be 10 digits!")
+                        }
+                        run(currentContacts); //send back to main
+                    });
+                }
+            });
+        } 
         else if(action.toLowerCase() === "search")
         {
             rl.question("Which Contact's information would you like to see? \n", (nameToFind) => {
